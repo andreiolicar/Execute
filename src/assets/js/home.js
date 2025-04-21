c3bfc159fe649ffff7149787d70c8ddeca57282b
Andrei <andreiolicar7@gmail.com>
2025-04-21 20:20:22 -0300

    responsividade completa do header para computadores menores e dispositivos mobile.

---
+++ src/assets/js/home.js
@@ -0,0 +1,26 @@
+function mobileHeader() {
+    const hamburger = document.querySelector('.hamburger');
+    const itensMobile = document.querySelector('.itens-mobile');
+
+    if (itensMobile.classList.contains('open')) {
+        itensMobile.classList.remove('open');
+
+        void itensMobile.offsetWidth;
+
+        itensMobile.classList.add('closing');
+
+        const handleAnimationEnd = () => {
+            itensMobile.classList.remove('closing');
+            itensMobile.style.display = 'none';
+            itensMobile.removeEventListener('animationend', handleAnimationEnd);
+        };
+
+        itensMobile.addEventListener('animationend', handleAnimationEnd);
+    } else {
+        itensMobile.style.display = 'flex';
+        void itensMobile.offsetWidth;
+        itensMobile.classList.add('open');
+    }
+
+    hamburger.classList.toggle('open');
+}
