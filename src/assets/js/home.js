4f523a8ad34bb07b8710010ed3375ade4ab2d614
Andrei <andreiolicar7@gmail.com>
2025-04-23 10:21:04 -0300

    finalizacao da logica do carousel de depoimentos (incompleto).

--- src/assets/js/home.js
+++ src/assets/js/home.js
@@ -27,6 +27,26 @@ function mobileHeader() {
 
 document.addEventListener('DOMContentLoaded', function () {
     const depo1 = document.querySelector('#depo1');
+    const depo2 = document.querySelector('#depo2');
+    const depo3 = document.querySelector('#depo3');
 
-    depo1.classList.toggle('active');
+    function carouselDepo() {
+        depo1.classList.toggle('active');
+        setTimeout(function () {
+            depo1.classList.remove('active');
+            depo2.classList.toggle('active');
+        }, 5000);
+
+        setTimeout(function () {
+            depo2.classList.remove('active');
+            depo3.classList.toggle('active');
+        }, 10000);
+
+        setTimeout(function () {
+            depo3.classList.remove('active');
+            depo1.classList.toggle('active');
+        }, 15000);
+    }
+
+    carouselDepo();
 })
